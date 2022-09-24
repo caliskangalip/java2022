eced1d91346ed4261822f9ee70f71392252c22a8
caliskangalip <63237443+caliskangalip@users.noreply.github.com>
2022-09-24 09:55:57 +0300

    ders 6-7-8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        /*
+        DERS5
+        System.out.println("Hello java!");
+
+        DERS 6 variables
+        int sayi=10;
+        String mesaj= "Öğrenci sayısı "
+        System.out.println(mesaj + sayi);
+        System.out.println(mesaj + sayi);
+        System.out.println(mesaj + sayi);
+        System.out.println(mesaj + sayi);
+
+        DERS 7 datatypes
+        int sayiInt = 1300000000;
+        short sayiShort = (short) 111111111;
+        byte sayiByte = 127;
+        long sayiLong = 92080;
+        double sayiDouble= 12.5;
+        boolean sayiBool = true;
+        char sayiChar = 'O';
+        String sayiString = "Beş";
+        System.out.println(sayiString+sayiInt+sayiShort+sayiByte+sayiLong+sayiDouble+sayiBool+sayiChar);
+
+        DERS 8 if blokları
+        int sayi= 20;
+        if (sayi>0){
+            System.out.println("Sayı sıfırdan büyük");
+        } else if (sayi == 20) {
+            System.out.println("Sayı tam yirmidir");
+        } else {
+            System.out.println("Sayı sıfırdan küçük");
+        }
     }
 }
