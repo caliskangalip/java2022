59e4413c0b250154c824187bc1afdd4a568d47f5
caliskangalip <63237443+caliskangalip@users.noreply.github.com>
2022-09-27 12:55:42 +0300

    loopDemo

---
+++ week01/loopDemo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        //For loop
+        for (int i=0;i<=10;i+=2){
+            System.out.println(i);
+        }
+        System.out.println("Döngü bitti");
+    }
+}
