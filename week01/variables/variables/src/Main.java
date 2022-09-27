ea5509c66f0f084ab1c1c096b4e00c1970e39e13
caliskangalip <63237443+caliskangalip@users.noreply.github.com>
2022-09-27 11:04:32 +0300

    variables

---
+++ week01/variables/variables/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayi=10;
+        String mesaj= "Öğrenci sayısı ";
+        System.out.println(mesaj + sayi);
+        System.out.println(mesaj + sayi);
+        System.out.println(mesaj + sayi);
+        System.out.println(mesaj + sayi);
+    }
+}
