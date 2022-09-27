938d537195c0b2f601e17b007f944ad9a7271a0d
caliskangalip <63237443+caliskangalip@users.noreply.github.com>
2022-09-27 12:09:44 +0300

    dataTypes

---
+++ week01/dataTypes1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int sayiInt = 1300000000;
+        short sayiShort = (short) 111111111;
+        byte sayiByte = 127;
+        long sayiLong = 92080;
+        double sayiDouble= 12.5;
+        boolean sayiBool = true;
+        char sayiChar = 'O';
+        String sayiString = "Beş";
+        System.out.println(sayiString+sayiInt+sayiShort+sayiByte+sayiLong+sayiDouble+sayiBool+sayiChar);
+    }
+}
