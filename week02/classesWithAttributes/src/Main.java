a01973311836f306b8e8a6879cdd18adc891880f
caliskangalip <63237443+caliskangalip@users.noreply.github.com>
2022-09-28 13:29:05 +0300

    getterAndSetter

--- week02/classesWithAttributes/src/Main.java
+++ week02/classesWithAttributes/src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         Product product=new Product();
-        product.name="Laptop";
-        product.id=1;
-        product.description="Asus Laptop";
-        product.price=5000;
-        product.stockAmount=3;
+        product.setName("Laptop");
+        product.setId(1);
+        product.setDescription("Asus Laptop");
+        product.setPrice(5000);
+        product.setStockAmount(3);
 
         ProductManager productManager=new ProductManager();
         productManager.add(product);
--- week02/classesWithAttributes/src/ProductManager.java
+++ week02/classesWithAttributes/src/ProductManager.java
@@ -1,5 +1,5 @@
 public class ProductManager {
     public void add(Product product){
-        System.out.println("Ürün eklendi : "+product.name);
+        System.out.println("Ürün eklendi : "+product.getName()+product.getKod());
     }
 }
