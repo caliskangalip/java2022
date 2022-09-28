c7bceddba27932bab0b30c24927fd0dc51682b4f
caliskangalip <63237443+caliskangalip@users.noreply.github.com>
2022-09-28 09:33:59 +0300

    methods

---
+++ week02/methods/src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        findNumber();
+
+    }
+    public static void findNumber(){
+        int[] numbers =new  int[]{1,2,5,7,9,0};
+        int willFind=6;
+        boolean isThere=false;
+        for (int number:numbers){
+            if (number==willFind){
+                isThere=true;
+                break;
+            }
+        }
+        if (isThere){
+            showMessage("This number is there in series!");
+        }else {
+            showMessage("This number is not there in series!");
+        }
+    }
+    public static void showMessage(String text){
+        System.out.println(text);
+    }
+}
