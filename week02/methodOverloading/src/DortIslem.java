ed67d760d83ee3934dd5135aaa9d361f7f02e5d2
caliskangalip <63237443+caliskangalip@users.noreply.github.com>
2022-09-28 16:12:00 +0300

    Java Dersi 36 : Inheritance Yapısını Anlamak

---
+++ week02/methodOverloading/src/DortIslem.java
@@ -0,0 +1,8 @@
+public class DortIslem {
+    public int topla(int number1, int number2){
+        return number1+number2;
+    }
+    public int topla(int number1, int number2, int number3){
+        return number1+number2+number3;
+    }
+}
